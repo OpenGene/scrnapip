3d3696ff5d933482756c4252a559ac354fee7038
zhangjing1123 <126152114+zhangjing1123@users.noreply.github.com>
2023-06-07 10:10:52 +0800

    Delete src directory

--- src/singleRdata/singleRdata/clusterprofiler/test.sh
+++
@@ -1,9 +0,0 @@
-#awk -F "\t" '{print $7}' test/All_common_snp.anno.vcf.hg19_multianno.xls > test/gene.lst
-#sort -u test/gene.lst > test/gene.xls
-#head -100 test/gene.xls > test/gene_to_go.xls
-/thinker/nfs3/public/wangwf/bin/anaconda3/envs/R3.4.3/bin/Rscript Msigdb2entrezID.R -f KEGG.MSigDB.txt -n KEGG_MsigDB_hsa_entrezID.xls -g 1 -t SYMBOL
-
-## add symbol id 
-python KEGG_id_convert.py
-awk -F "\t" '{print $2"\t"$4"\t"$3}' KEGG_HAS.txt  > KEGG_HAS_PATHWAY.txt
-##KEGG_HAS_PATHWAY.txt used for pathway annotations
